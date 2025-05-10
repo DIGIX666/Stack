fd4af9ea56f6ca44dc792777d35e854220a6780f
dossantos-lukombo <dossantos.lukombo918@gmail.com>
2025-05-10 19:36:10 +0200

    chore: adding CI/CD for github with cd.yml et ci.yml files

--- migrations/0001_create_users.sql
+++ migrations/0001_create_users.sql
@@ -1,11 +1,11 @@
 -- Table users
 CREATE TABLE users (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  username TEXT NOT NULL UNIQUE,
-  email TEXT NOT NULL UNIQUE,
-  password_hash TEXT NOT NULL,
-  avatar_url TEXT,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+    id UUID PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    avatar_url TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
